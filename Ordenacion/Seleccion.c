d6f2da9b33302fbb5c1c3894f1d0952a95281181
Javier Martin <9404008@alumnos.ufv.es>
2025-12-03 11:22:30 +0100

    Por obra en selección hecho

--- Ordenacion/Seleccion.c
+++ Ordenacion/Seleccion.c
@@ -5,7 +5,7 @@
  * Descripción: Ordena el array de fichas de libros en un doble puntero a las fichas de Libros,
  *              por lo que el movimient de datos se reduce al cambio de punteros.
  *              Se usa el algoritmo de ordenación de Selección.
- * Reglas de uso: 
+ * Reglas de uso:
  * Código de Retorno: LIBRO **: Doble puntero ordenado por el criterio pedido a las fichas de libros.
  * Programador: JMSM (Sept-24)
  *****************************************/
@@ -16,16 +16,75 @@ DISCO **Seleccion(DISCO *Ficha, int Campo)
 {
     DISCO **Orden;
     struct timeval inicio, fin;
-    
-    //Añade aquí la definición del resto de variables necesarias
 
-    gettimeofday(&inicio,NULL);
-    Orden=InitOrden(Ficha);
+    // Añade aquí la definición del resto de variables necesarias
+    char *min;
+    char *comparador;
+    int indiceMenor;
+    DISCO *obraMenor;
 
-    //Código del Alumno del Algoritmo de ordenación por Selección
+    gettimeofday(&inicio, NULL);
+    Orden = InitOrden(Ficha);
 
-    gettimeofday(&fin,NULL);
-    Estadisticas.TiempoSeleccion=DifTiempo(inicio,fin);
+    // Código del Alumno del Algoritmo de ordenación por Selección
+    if (Campo == ORDEN_POR_TITULO)
+    {
+        for (int i = 0; i < Estadisticas.NumeroFichas - 1; i++)
+        {
+            obraMenor = Orden[i];
+            min = Orden[i]->Obra;
+            indiceMenor = i;
+            for (int j = i + 1; j < Estadisticas.NumeroFichas; j++)
+            {
+                comparador = Orden[j]->Obra;
+                if (strcmp(min, comparador) > 0)
+                {
+                    obraMenor = Orden[j];
+                    indiceMenor = j;
+                    min = Orden[j]->Obra;
+                }
+            }
+            Orden[indiceMenor] = Orden[i];
+            Orden[i] = obraMenor;
+        }
+    }
+    else if (Campo == ORDEN_POR_AUTOR)
+    {
+        for (int i = 0; i < Estadisticas.NumeroFichas - 1; i++)
+        {
+           obraMenor = Orden[i];
+            min = Orden[i]->ApellAutor;
+            indiceMenor = i;
+            for (int j = i + 1; j < Estadisticas.NumeroFichas; j++)
+            {
+                comparador = Orden[j]->ApellAutor;
+                if (strcmp(min, comparador) > 0)
+                {
+                    obraMenor = Orden[j];
+                    indiceMenor = j;
+                    min = Orden[j]->ApellAutor;
+                }
+                else if (strcmp(min, comparador) == 0)
+                {
 
-    return(Orden);
+                    
+                    min = Orden[i]->NomAutor;
+                    comparador = Orden[j] ->NomAutor;
+                    if (strcmp(min, comparador) > 0)
+                    {
+                        obraMenor = Orden[j];
+                        indiceMenor = j;
+                        min = Orden[j]->NomAutor;
+                    }
+                }
+            }
+            Orden[indiceMenor] = Orden[i];
+            Orden[i] = obraMenor;
+
+        }
+    }
+    gettimeofday(&fin, NULL);
+    Estadisticas.TiempoSeleccion = DifTiempo(inicio, fin);
+
+    return (Orden);
 }
