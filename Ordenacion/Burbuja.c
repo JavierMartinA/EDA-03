db81a11b11512a82f18847b1658c452b148b7c93
Miguel <9401997@alumnos.ufv.es>
2025-12-07 11:11:25 +0400

    compara por obras en burbuja

--- Ordenacion/Burbuja.c
+++ Ordenacion/Burbuja.c
@@ -5,27 +5,50 @@
  * Descripción: Ordena el array de fichas de libros en un doble puntero a las fichas de Libros,
  *              por lo que el movimient de datos se reduce al cambio de punteros.
  *              Se usa el algoritmo de ordenación de la burbuja.
- * Reglas de uso: 
+ * Reglas de uso:
  * Código de Retorno: LIBRO **: Doble puntero ordenado por el criterio pedido a las fichas de libros.
  * Programador: JMSM (Sept-24)
  *****************************************/
 
 #include "../Discoteca.h"
 
-DISCO **Burbuja(DISCO *Ficha,int Campo)
+DISCO **Burbuja(DISCO *Ficha, int Campo)
 {
     DISCO **Orden;
     struct timeval inicio, fin;
 
-    //Añade aquí la definición del resto de variables necesarias
-    
-    gettimeofday(&inicio,NULL);
-    Orden=InitOrden(Ficha);
+    // Añade aquí la definición del resto de variables necesarias
 
-    //Código del Alumno del método de ordenación de la Burbuja
-    
-    gettimeofday(&fin,NULL);
-    Estadisticas.TiempoBurbuja=DifTiempo(inicio,fin);
+    gettimeofday(&inicio, NULL);
+    Orden = InitOrden(Ficha);
+    DISCO aux;
+    // Código del Alumno del método de ordenación de la Burbuja
 
-    return(Orden);
+    gettimeofday(&fin, NULL);
+    Estadisticas.TiempoBurbuja = DifTiempo(inicio, fin);
+
+    if (Campo == ORDEN_POR_AUTOR)
+    {
+    }
+    else
+    {
+        for (int i = 1; i <= Estadisticas.NumeroFichas - 1; i++)
+        {
+            // Coge los N-i primeros elementos y los compara
+            // cada uno de ellos con el inmediatamente posterior.
+            for (int j = 0; j < Estadisticas.NumeroFichas - i; j++)
+            {
+                // si están desordenados se intercambian
+                // usando una variable auxiliar.
+                if (strcmp(Ficha[j].Obra,Ficha[j + 1].Obra) > 0)
+                {
+                    aux = Ficha[j];
+                    Ficha[j] = Ficha[j + 1];
+                    Ficha[j + 1] = aux;
+                }
+            }
+        }
+    }
+
+    return (Orden);
 }
