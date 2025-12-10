b2ea8c38bda11d07fc95908760bffa810f3fc21d
Javier Martin <9404008@alumnos.ufv.es>
2025-12-10 09:47:38 +0100

    Cambiado a Orden

--- Ordenacion/Insercion.c
+++ Ordenacion/Insercion.c
@@ -22,7 +22,7 @@ DISCO **Insercion(DISCO *Ficha,int Campo)
     gettimeofday(&inicio,NULL);
     Orden=InitOrden(Ficha);
 
-    DISCO aux;
+    DISCO* aux;
     char *nombreMenor;
     char * nombreComparador;
 
@@ -37,33 +37,33 @@ DISCO **Insercion(DISCO *Ficha,int Campo)
         for (int i = 1; i < Estadisticas.NumeroFichas; i++)
         {
             // Almacenar el elemento actual que se insertará en la secuencia ordenada
-            aux = Ficha[i]; 
+            aux = Orden[i]; 
             int j = i - 1; // Iniciar la comparación con el elemento inmediatamente anterior
 
             // Bucle interno: Mueve los elementos de la matriz ordenada (de 0 a i-1) 
             //que son mayores que la clave a una posición por delante de su posición actual.
             while (j >= 0)
             {
-                int comparison = strcmp(Ficha[j].ApellAutor, aux.ApellAutor);
+                int comparison = strcmp(Orden[j]->ApellAutor, aux->ApellAutor);
 
                 //Si el elemento en la parte ordenada (Ficha[j]) es MAYOR
                 //que el apellido de la clave (aux.ApellAutor), desplazar Ficha[j] a la derecha.
                 if (comparison > 0)
                 {
-                    Ficha[j + 1] = Ficha[j];
+                    Orden[j + 1] = Orden[j];
                     j--;
                 }
                 // si los apellidos son IGUALES (empate), verifique el nombre.
                 else if (comparison == 0)
                 {
                     // Comprobación de seguridad para NULL NomAutor antes de strcmp
-                    if (Ficha[j].NomAutor == NULL) Ficha[j].NomAutor = "";
-                    if (aux.NomAutor == NULL) aux.NomAutor = "";
+                    if (Orden[j]->NomAutor == NULL) Orden[j]->NomAutor = "";
+                    if (aux->NomAutor == NULL) aux->NomAutor = "";
                 
                     // Si el elemento en el primer nombre de la parte ordenada es MAYOR, desplácelo a la derecha.
-                    if (strcmp(Ficha[j].NomAutor, aux.NomAutor) > 0)
+                    if (strcmp(Orden[j]->NomAutor, aux->NomAutor) > 0)
                     {
-                        Ficha[j + 1] = Ficha[j];
+                        Orden[j + 1] = Orden[j];
                         j--;
                     }
                     // Si los apellidos son iguales Y los nombres NO son mayores (es decir, menores o iguales),
@@ -80,7 +80,7 @@ DISCO **Insercion(DISCO *Ficha,int Campo)
                 }
             }
             //Colocar el elemento adonde pertenece
-            Ficha[j + 1] = aux;
+            Orden[j + 1] = aux;
         }
     }
     // ---
@@ -90,17 +90,17 @@ DISCO **Insercion(DISCO *Ficha,int Campo)
         for (int i = 1; i < Estadisticas.NumeroFichas; i++)
         {
             // Almacena el elemento actual que se insertará
-            aux = Ficha[i];
+            aux = Orden[i];
             int j = i - 1; 
 
             // Bucle interno: mover los elementos que son mayores que la clave una posición hacia adelante.
-            while (j >= 0 && strcmp(Ficha[j].Obra, aux.Obra) > 0)
+            while (j >= 0 && strcmp(Orden[j]->Obra, aux->Obra) > 0)
             {
-                Ficha[j + 1] = Ficha[j];
+                Orden[j + 1] = Orden[j];
                 j--;
             }
             // Inserte la llave en su posición correcta
-            Ficha[j + 1] = aux;
+            Orden[j + 1] = aux;
         }
     }
     gettimeofday(&fin,NULL);
